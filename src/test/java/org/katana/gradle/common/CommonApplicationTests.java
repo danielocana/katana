38ee079012f53dea39f6f0c9b543a1ce6f52d17f
danielocana <danyh.88@gmail.com>
2019-02-24 16:26:12 -0500

    add travis

--- src/test/java/org/katana/gradle/common/CommonApplicationTests.java
+++ src/test/java/org/katana/gradle/common/CommonApplicationTests.java
@@ -1,10 +1,13 @@
 package org.katana.gradle.common;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import testcategories.IntegrationTest;
 
+@Category(IntegrationTest.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CommonApplicationTests {
@@ -12,5 +15,4 @@ public class CommonApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
 }
---
+++ src/test/java/org/katana/gradle/common/InitialTest.java
@@ -0,0 +1,16 @@
+package org.katana.gradle.common;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import testcategories.UnitaryTest;
+
+import static org.junit.Assert.assertTrue;
+
+@Category(UnitaryTest.class)
+public class InitialTest {
+
+    @Test
+    public void testOne () {
+        assertTrue(true);
+    }
+}
